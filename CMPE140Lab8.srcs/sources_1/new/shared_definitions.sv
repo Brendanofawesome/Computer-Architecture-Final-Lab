2be090e21169a97c137c6c9a50aec269b13ecae5
Brendan <brendanparvin7@gmail.com>
2026-04-08 09:13:06 -0700

    Created the shared definitions package

---
+++ CMPE140Lab8.srcs/sources_1/new/shared_definitions.sv
@@ -0,0 +1,12 @@
+//defines the list of shared definitions that can be used by all modules
+
+package shared_definitions_pkg;
+    ////////////////
+    // ENUM TYPES //
+    ////////////////
+
+    //instruction format codes
+    typedef enum {INSTR_I_FORMAT, INSTR_J_FORMAT, INSTR_R_FORMAT} instruction_format_e;
+
+    
+endpackage
