fbf9635af8f21bd483a0b3c00625c00709621550
Brendan <brendanparvin7@gmail.com>
2026-04-27 11:57:43 -0700

    minor fixes

--- CMPE140Lab8.srcs/sources_1/new/branch_precalc.sv
+++ CMPE140Lab8.srcs/sources_1/new/branch_precalc.sv
@@ -1,13 +1,13 @@
 module branch_precalc (
-    input  logic [31:2] src1,     
-    input  logic [15:0] src2,     
- 
-    output logic [31:2] q         
+    input  logic [31:2] src1,
+    input  logic [15:0] src2,
+
+    output logic [31:2] q
 );
- 
-    logic signed [29:0] imm_sext;
+
+    logic signed [31:2] imm_sext;
     assign imm_sext = {{14{src2[15]}}, src2[15:0]};
- 
+
     assign q = $signed(src1) + imm_sext;
- 
+
 endmodule
